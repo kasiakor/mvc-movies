3fd752912e15b5cd42a76d1d42472f78bea78530
Kasia Kordys <kasia.kordys@gmail.com>
2020-02-17 15:17:20 +0000

    Add Welcome action method and view and pass data using ViewBag

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -18,9 +18,12 @@ namespace MvcMovie.Controllers
         //
         // GET: /HelloWorld/Welcome/
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("This is the Welcome action method..." + name + " " + ID);
+            //return HttpUtility.HtmlEncode("This is the Welcome action method..." + name + " " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
         }
     }
 }
